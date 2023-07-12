8ccf084b8eb9a3604f8e2f2b9c9b503b5b6e4250
rohan <rohankhadka33@gmail.com>
2023-07-12 14:31:40 +0545

    current time stock dashboard

--- views.py
+++ views.py
@@ -46,3 +46,27 @@ def index(request):
     data.reset_index(level=0, inplace=True)
 
 
+
+    fig_left = go.Figure()
+    fig_left.add_trace(
+                go.Scatter(x=data['Date'], y=data['AAPL']['Adj Close'], name="AAPL")
+            )
+    fig_left.add_trace(
+                go.Scatter(x=data['Date'], y=data['AMZN']['Adj Close'], name="AMZN")
+            )
+    fig_left.add_trace(
+                go.Scatter(x=data['Date'], y=data['QCOM']['Adj Close'], name="QCOM")
+            )
+    fig_left.add_trace(
+                go.Scatter(x=data['Date'], y=data['META']['Adj Close'], name="META")
+            )
+    fig_left.add_trace(
+                go.Scatter(x=data['Date'], y=data['NVDA']['Adj Close'], name="NVDA")
+            )
+    fig_left.add_trace(
+                go.Scatter(x=data['Date'], y=data['JPM']['Adj Close'], name="JPM")
+            )
+    fig_left.update_layout(paper_bgcolor="#14151b", plot_bgcolor="#14151b", font_color="white")
+
+    plot_div_left = plot(fig_left, auto_open=False, output_type='div')
+
